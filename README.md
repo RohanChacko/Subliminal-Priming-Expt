ade3681703961cbe1ce72882f227cf522f2d0534
rohanchacko <rohanchacko@github.com>
2019-11-09 22:48:23 +0530

    Initial commit. Adding template code for expt and Logos dir

---
+++ README.md
@@ -0,0 +1,23 @@
+# EXPERIMENT 1
+
+## TEMPORAL FRAME ORDER
+* First frame : White background with black fixation point in center of window. Use parameters as used below. Duration [500 ms]
+
+* Second frame : White background with non-informative stimulus => red/green circle with radius a bit larger than fixation point. Center of screen. Duration [850 ms]
+
+* Third frame : Same first frame for 500 ms
+
+* Fourth frame : Black fixation point in the center. Two colored circles (red/green) on either side of the point with centeres lying on the same line as that of the fixation point. Mix the spatial order of the stimuli (if performing TOJ). We can test if participant registers the non-informative stimuli by doing a variant of this. Call to discuss
+
+
+## USER ACTIVITY
+* On fourth frame, ask user to perform simultaneity judgement task (SJ) or temporal order judgement task (TOJ).
+
+* Recommended => SJ task as it reduces response bias. Call me to discuss this detail
+
+* Hypothesis : Attention will be directed to the stimulus primed by the non-informative stimulus prior
+
+
+# EXPERIMENT 2
+
+* Expt 1 with Images as prior
---
+++ expt_1.0.py
@@ -0,0 +1,66 @@
+import pygame
+import time
+
+## Ripped off AadilMehdi Sanchawala...like always
+
+WIDTH = 1000
+HEIGHT = 1000
+FPS = 10
+
+# Define Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+## Initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Expt 1.0")
+clock = pygame.time.Clock()
+
+
+def render() :
+    '''
+    render the sequence of frames for experiment 1
+    '''
+
+    screen.fill(BLACK)
+
+
+    # Show a white circle initially
+    pygame.draw.circle(screen, WHITE, (HEIGHT//2, WIDTH//2), 10, 2)
+    pygame.display.update()
+
+    # pygame delay does not do what we want
+    pygame.time.delay(150)
+    screen.fill(BLACK)
+
+
+
+
+
+def main() :
+    ## Game Loop
+    running = True
+
+    while running:
+
+        clock.tick(FPS)     ## will make the loop run at the same speed all the time
+        for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
+
+            ## listening for the the X button at the top or press escape
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) :
+                running = False
+
+        render()
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__' :
+    main()
